ad450ab2313682bea2ae6081afec41d8e2de3830
Parth <parthmshah1302@gmail.com>
2020-10-21 04:53:18 +0530

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function check(){
     var q1=document.myform.q1.value;
     var q2=document.myform.q2.value;
     var q3=document.myform.q3.value;
+    var q4=document.myform.q4.value;
+    var q5=document.myform.q5.value;
     
     var counter=0;
     if(q1=="a"){
@@ -16,8 +18,8 @@ function check(){
     if(q4=="d"){
         counter++;
     }
-    if(q4=="c"){
+    if(q5=="c"){
         counter++;
     }
-    alert("Your score is "+counter+"/3 marks.")
+    alert("Your score is "+counter+"/5 marks.")
 }
